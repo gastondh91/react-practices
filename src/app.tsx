c8d30ff30646f1825e527a15fda8263dd69f322c
Gastón d'Hiriart <gastondh91@gmail.com>
2022-08-27 19:38:24 -0300

    min changes

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import MainComponent from './components/MainComponent'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>React app</React.StrictMode>
+  <React.StrictMode>
+    <MainComponent />
+  </React.StrictMode>
 )
---
+++ src/components/MainComponent.tsx
@@ -0,0 +1,7 @@
+const MainComponent = () => {
+  console.log('App initialized')
+
+  return <>React app</>
+}
+
+export default MainComponent
